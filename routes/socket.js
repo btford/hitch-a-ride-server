a4b3307ecc260fbebd266867a5da83ea1959e182
Brian Ford <btford@umich.edu>
2013-03-16 12:44:01 -0400

    add socket API for matching

--- routes/socket.js
+++ routes/socket.js
@@ -2,14 +2,63 @@
  * Serve content over a socket
  */
 
-module.exports = function (socket) {
-  socket.emit('send:name', {
-    name: 'Bob'
+var _ = require('lodash');
+
+var riders = {};
+var drivers = {};
+
+var canHitchRide = require('../lib/can-hitch-ride');
+
+var checkMatches = function () {
+
+  _.each(drivers, function (driver) {
+    _.each(riders, function (rider) {
+      canHitchRide({
+        driver: driver,
+        rider: rider
+      }, function (canHitch) {
+        console.log(driver.from, rider.from);
+        if (canHitch &&
+            drivers[driver.socket.id] &&
+            riders[rider.socket.id]) {
+
+          delete drivers[driver.socket.id];
+          delete riders[rider.socket.id];
+
+          driver.socket.emit('trip:matched');
+          rider.socket.emit('trip:matched');
+
+          console.log('matched!');
+        } else {
+          console.log('not matched!');
+        }
+      });
+    });
   });
 
-  setInterval(function () {
-    socket.emit('send:time', {
-      time: (new Date()).toString()
-    });
-  }, 1000);
+};
+
+module.exports = function (socket) {
+
+  socket.on('send:rider:trip', function (data, fn) {
+    console.log(data);
+    riders[socket.id] = {
+      socket: socket,
+      from: data.from,
+      to: data.to
+    };
+    fn();
+    checkMatches();
+  });
+
+  socket.on('send:driver:trip', function (data, fn) {
+    console.log(data);
+    drivers[socket.id] = {
+      socket: socket,
+      from: data.from,
+      to: data.to
+    };
+    fn();
+    checkMatches();
+  });
 };
